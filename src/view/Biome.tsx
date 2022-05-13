d2234f10916b36328264febb9361e74ce9834fbc
Kirill Sukhomlin <ya@kirilloid.ru>
2022-05-14 02:09:07 +0300

    minor biome page refactoring

--- src/view/Biome.tsx
+++ src/view/Biome.tsx
@@ -3,11 +3,12 @@ import { Link, useParams } from 'react-router-dom';
 
 import '../css/Biome.css';
 
-import type { EntityId, GameObject, Item, PhysicalObject, Resource, Structure } from '../types';
+import type { Biome, BiomeConfig, EntityId, GameObject, Item, PhysicalObject, Resource, Structure } from '../types';
 import { biomes } from '../data/location';
 import { maxLvl } from '../data/creatures';
 import { data } from '../data/itemDB';
 import { resourceCraftMap } from '../data/resource-usage';
+import { envSetup } from '../data/env';
 
 import { TranslationContext } from '../effects';
 import { averageAttacksDamage, InlineObjectWithIcon, yesNo } from './helpers';
@@ -42,18 +43,22 @@ function isFoodOrUsedForFood(item: Item) {
   return false;
 }
 
-export function Biome() {
-  const { id } = useParams<{ id: string }>();
+function Weather({ biome }: { biome: BiomeConfig }) {
   const translate = useContext(TranslationContext);
+  const envs = envSetup[biome.id];
+  const totalWeight = envs.reduce((total, [, weight]) => weight + total, 0);
+  return <section>
+    <h2><Link to="/weather">{translate('ui.page.weather')}</Link></h2>
+    <dl>
+      {envs.map(([env, weight]) => <React.Fragment key={env}>
+        <dt>{translate(`ui.weather.${env}`)}</dt><dd>{Math.round(weight / totalWeight * 100)}%</dd>
+      </React.Fragment>)}
+    </dl>
+  </section>
+}
 
-  const biome = biomes.find(b => b.id === id);
-  if (biome == null) {
-    return <span className="error">
-      Biome "{id}" not found
-    </span>
-  }
-
-  const imgPath = `/icons/bg/${id}`;
+function Resources({ biome }: { biome: BiomeConfig }) {
+  const translate = useContext(TranslationContext);
 
   const resources = {
     trophies: [] as Item[],
@@ -66,7 +71,7 @@ export function Biome() {
   for (const res of biome.resources) {
     const item = data[res];
     if (item == null) {
-      console.error(`Resource '${res}' from biome '${id}' not found`);
+      console.error(`Resource '${res}' from biome '${biome.id}' not found`);
       continue;
     }
     if (item.type === 'piece' || item.type === 'creature') continue;
@@ -98,6 +103,93 @@ export function Biome() {
     }
   }
 
+  return <section>
+    <h2>{translate('ui.resources')}</h2>
+    <div className="multiList">
+      <div>
+        <h3>food</h3>
+        <ResourceList list={resources.food} />
+      </div>
+      <div>
+        <h3>{translate('ui.resources')}</h3>
+        <ResourceList list={resources.others} />
+      </div>
+      <div>
+        <h3>{translate('ui.trophies')}</h3>
+        <ResourceList list={resources.trophies} />
+      </div>
+    </div>
+    <h2>{translate('ui.mining')}</h2>
+    <div className="multiList">
+      <div>
+        <h3>{translate('ui.mineType.miscs')}</h3>
+        <ResourceList list={resources.misc} />
+      </div>
+      <div>
+        <h3>{translate('ui.mineType.trees')}</h3>
+        <ResourceList list={resources.tree} />
+      </div>
+      <div>
+        <h3>{translate('ui.mineType.rocks')}</h3>
+        <ResourceList list={resources.rock} />
+      </div>
+    </div>
+  </section>
+}
+
+function Creatures({ biome }: { biome: BiomeConfig }) {
+  const translate = useContext(TranslationContext);
+  return <section>
+    <h2>{translate('ui.creatures')}</h2>
+    <table width="100%">
+      <thead>
+        <tr>
+          <th></th>
+          <th>{translate('ui.creature')}</th>
+          <th>{translate('ui.health')}</th>
+          <th>{translate('ui.damage')}</th>
+          <th>⭐</th>
+        </tr>
+      </thead>
+      <tbody>
+        {biome.creatures.map(c =>
+        <tr key={c.id}>
+          <td><ItemIcon item={c} size={32} /></td>
+          <td><Link to={`/obj/${c.id}`}>{translate(c.id)}</Link></td>
+          <td className="value">{c.hp}</td>
+          <td className="value">{averageAttacksDamage(c) || '—'}</td>
+          <td className="value">{yesNo(maxLvl(c) > 1)}</td>
+        </tr>)}
+      </tbody>
+    </table>
+  </section>
+}
+
+function Locations({ biome }: { biome: BiomeConfig }) {
+  const translate = useContext(TranslationContext);
+  return <section>
+    <h2>{translate('ui.locations')}</h2>
+    <ul className="plainList">{biome.locations.map(id =>
+      <li key={id}>
+        <Link to={`/loc/${id}`}>{translate(`ui.location.${id}`)}</Link>
+      </li>)}
+    </ul>
+  </section>
+}
+
+export function Biome() {
+  const { id } = useParams<{ id: string }>();
+  const translate = useContext(TranslationContext);
+
+  const biome = biomes.find(b => b.id === id);
+  if (biome == null) {
+    return <span className="error">
+      Biome "{id}" not found
+    </span>
+  }
+
+  const imgPath = `/icons/bg/${id}`;
+
   return (
     <>
       <SpoilerAlert tier={biome.tier} />
@@ -112,70 +204,10 @@ export function Biome() {
           <dt>tier</dt><dd>{biome.tier}</dd>
         </dl>
       </section>
-      <section>
-        <h2>{translate('ui.resources')}</h2>
-        <div className="multiList">
-          <div>
-            <h3>food</h3>
-            <ResourceList list={resources.food} />
-          </div>
-          <div>
-            <h3>{translate('ui.resources')}</h3>
-            <ResourceList list={resources.others} />
-          </div>
-          <div>
-            <h3>{translate('ui.trophies')}</h3>
-            <ResourceList list={resources.trophies} />
-          </div>
-        </div>
-        <h2>{translate('ui.mining')}</h2>
-        <div className="multiList">
-          <div>
-            <h3>{translate('ui.mineType.miscs')}</h3>
-            <ResourceList list={resources.misc} />
-          </div>
-          <div>
-            <h3>{translate('ui.mineType.trees')}</h3>
-            <ResourceList list={resources.tree} />
-          </div>
-          <div>
-            <h3>{translate('ui.mineType.rocks')}</h3>
-            <ResourceList list={resources.rock} />
-          </div>
-        </div>
-      </section>
-      <section>
-        <h2>{translate('ui.creatures')}</h2>
-        <table width="100%">
-          <thead>
-            <tr>
-              <th></th>
-              <th>{translate('ui.creature')}</th>
-              <th>{translate('ui.health')}</th>
-              <th>{translate('ui.damage')}</th>
-              <th>⭐</th>
-            </tr>
-          </thead>
-          <tbody>
-            {biome.creatures.map(c =>
-            <tr key={c.id}>
-              <td><ItemIcon item={c} size={32} /></td>
-              <td><Link to={`/obj/${c.id}`}>{translate(c.id)}</Link></td>
-              <td className="value">{c.hp}</td>
-              <td className="value">{averageAttacksDamage(c) || '—'}</td>
-              <td className="value">{yesNo(maxLvl(c) > 1)}</td>
-            </tr>)}
-          </tbody>
-        </table>
-      </section>
-      <section>
-        <h2>{translate('ui.locations')}</h2>
-        <ul className="plainList">{biome.locations.map(id =>
-          <li key={id}>
-            <Link to={`/loc/${id}`}>{translate(`ui.location.${id}`)}</Link>
-          </li>)}
-        </ul>
-      </section>
+      <Weather biome={biome} />
+      <Resources biome={biome} />
+      <Creatures biome={biome} />
+      <Locations biome={biome} />
     </>
   );
 }
